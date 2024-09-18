a02b57dfc1c26e376b96abcbb56042be8030dcc8
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-09-18 23:45:08 +0530

    Navigate between routes

--- app/src/main/java/dizzcode/com/cupcake/CupcakeScreen.kt
+++ app/src/main/java/dizzcode/com/cupcake/CupcakeScreen.kt
@@ -2,6 +2,9 @@
 package dizzcode.com.cupcake
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,11 +19,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import dizzcode.com.cupcake.data.DataSource
+import dizzcode.com.cupcake.ui.OrderSummaryScreen
 import dizzcode.com.cupcake.ui.OrderViewModel
+import dizzcode.com.cupcake.ui.SelectOptionScreen
+import dizzcode.com.cupcake.ui.StartOrderScreen
+
+/**
+ * Start: Select the quantity of cupcakes from one of three buttons.
+ * Flavor: Select the flavor from a list of choices.
+ * Pickup: Select the pickup date from a list of choices.
+ * Summary: Review the selections and either send or cancel the order.
+ */
+enum class CupcakeScreen(){
+    Start,
+    Flavor,
+    Pickup,
+    Summary
+}
 
 /**
  * Composable that displays the topBar and displays back button if back navigation is possible.
@@ -68,5 +92,122 @@ fun CupcakeApp(
     ) {  innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
+        /**
+         * Note : 01
+         * NavHostController is a subclass of the NavController class
+         * that provides additional functionality for use with a NavHost composable.
+         */
+
+        /**
+         * Note : 02
+         * NavHost
+         *
+         * 1) navController:
+         * An instance of the NavHostController class.
+         * You can use this object to navigate between screens,
+         * for example, by calling the navigate() method to navigate to another destination.
+         * You can obtain the NavHostController by calling rememberNavController()
+         * from a composable function.
+         *
+         * 2) startDestination:
+         * A string route defining the destination shown by default when the
+         * app first displays the NavHost.
+         * In the case of the Cupcake app, this should be the Start route.
+         */
+
+        /**
+         * Note : 03
+         * Handle routes in your NavHost | composable()
+         *
+         * The composable() function has two required parameters.
+         *(The composable() function is an extension function of NavGraphBuilder.)
+         *
+         * 1) route:
+         * A string corresponding to the name of a route.
+         * This can be any unique string.
+         * You'll use the name property of the CupcakeScreen enum's constants.
+         *
+         * 2) content:
+         * Here you can call a composable that you want to display for the given route.
+         */
+        NavHost(
+            navController = navController,
+            startDestination = CupcakeScreen.Start.name,
+            modifier = Modifier.padding(innerPadding)
+        ){
+
+            composable( route = CupcakeScreen.Start.name){
+                StartOrderScreen(
+                    quantityOptions = DataSource.quantityOptions,
+                    onNextButtonClicked = {
+                        viewModel.setQuantity(it)
+                        navController.navigate(CupcakeScreen.Flavor.name)
+                    },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(dimensionResource(id = R.dimen.padding_medium))
+                )
+            }
+
+            composable( route = CupcakeScreen.Flavor.name){
+                val context = LocalContext.current
+
+                SelectOptionScreen(
+                    subtotal = uiState.price,
+                    onNextButtonClicked = { navController.navigate(CupcakeScreen.Pickup.name) },
+                    onCancelButtonClicked = {},
+                    options = DataSource.flavors.map { id -> context.resources.getString(id) },
+                    onSelectionChanged = { viewModel.setFlavor(it)},
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+
+            composable( route = CupcakeScreen.Pickup.name){
+                SelectOptionScreen(
+                    subtotal = uiState.price,
+                    onNextButtonClicked = { navController.navigate(CupcakeScreen.Summary.name) },
+                    onCancelButtonClicked = {},
+                    options = uiState.pickupOptions,
+                    onSelectionChanged = { viewModel.setDate(it) },
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+
+            composable( route = CupcakeScreen.Summary.name){
+                OrderSummaryScreen(
+                    orderUiState = uiState,
+                    onCancelButtonClicked = {},
+                    onSendButtonClicked = { subject: String, summary: String ->
+
+                    },
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+
+        }
     }
+}
+
+private fun cancelOrderAndNavigateToStart(
+    viewModel: OrderViewModel,
+    navController: NavHostController
+) {
+    viewModel.resetOrder()
+    navController.popBackStack(route = CupcakeScreen.Start.name, inclusive = false)
+    /**
+     * Note : 04
+     * Pop to the start screen
+     *
+     * The popBackStack() method has two required parameters.
+     *
+     * 1) route:
+     * The string representing the route of the destination you want to navigate back to.
+     *
+     * 2) inclusive:
+     * A Boolean value that,
+     * if true, also pops (removes) the specified route.
+     * If false, popBackStack() will remove all destinations on top of—but not
+     * including—the start destination, leaving it as the topmost screen visible
+     * to the user.
+     */
 }
